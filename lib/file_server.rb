fc5ee7371921798766113c98a627fd66b56ca1a2
LFDM <1986gh@gmail.com>
2014-03-24 09:43:51 +0100

    Quick implementation of FileServer

--- lib/file_server.rb
+++ lib/file_server.rb
@@ -1,5 +1,15 @@
 require "file_server/version"
 
 module FileServer
-  # Your code goes here...
+  require 'sinatra/base'
+
+  class Api < Sinatra::Base
+    set :public_folder, File.expand_path('.')
+    set :server, :puma
+
+    get '/' do
+      # redirect to a template that renders
+      # the contents of the directory
+    end
+  end
 end
